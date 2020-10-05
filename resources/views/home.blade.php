29d64d68ad6b3021488899c566d1540177bc7f05
Andres Veliz <veliz2022@gmail.com>
2020-10-05 17:32:06 -0400

    configurando el ambiente, plantillaAdminLte, login inicial

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('adminlte::page')
+
+@section('title', 'Elashes')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <div id="app">
+        <example-component> </example-component>
+    </div>
+@stop
+
+@section('css')
+    
+    
+@stop
+
+@section('js')
+    
+    
+@stop
---
+++ resources/views/trabajos/index.blade.php
@@ -0,0 +1,21 @@
+@extends('adminlte::page')
+
+@section('title', 'Elashes')
+
+@section('content_header')
+    <h1>hola trabajos</h1>
+@stop
+
+@section('content')
+<example-component> </example-component>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+    
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+    
+@stop
